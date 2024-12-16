b33d18108bee551d9158d3969970c30f976eb60e
sahilduatechkat <sahyldua@gmail.com>
2024-12-16 21:32:53 +0530

    Update IntroSection.tsx
    copy changes

--- components/Homepage/IntroSection.tsx
+++ components/Homepage/IntroSection.tsx
@@ -10,7 +10,7 @@ export const IntroSection = () => {
         <div className='flex flex-col-reverse lg:flex-row lg:items-start xl:items-center py-10 pt-4 px-6 lg:px-[100px] gap-6'>
             <article className='mt-[28px] lg:mt-auto'>
                 <h2 className={` text-[28px] leading-[40px] w-[90%] lg:text-[48px] lg:w-full lg:leading-tight xl:text-[60px] font-bold xl:w-[80%] xl:leading-[68px]`}>
-                    Digital Solutions That Drive Growth and Transform Futures
+                    Solutions That Drive Growth and Transform Businesses
                 </h2>
                 <p className={`text-sm lg:text-lg xl:text-xl xl:w-[80%] mt-3 lg:mt-10`}> 
                     We design, build, and deliver cutting-edge websites, apps, and strategies to empower businesses in the digital age. Let’s create something extraordinary together.
