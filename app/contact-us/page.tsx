4ef7b6701ca88b0cb4ffaa472810cdfa53aa8687
sahilduatechkat <sahyldua@gmail.com>
2025-03-10 23:44:14 +0530

    Update page.tsx
    address update

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -33,13 +33,13 @@ export default function ContactUsPage() {
                             119 Broadway, FL 1, Jersey City, New Jersey 07306 United States
                         </p>
                         <p className='mt-3 text-sm text-[#212121]'>
-                            First Floor, Plot No.9, Sector 115, Noida Uttar Pradesh 201301 India
+                            First Floor, Plot No.9, Sector 115, Noida, Uttar Pradesh 201301 India
                         </p>
                         
                     </ContactBulletPoint>
                     <ContactBulletPoint className="mb-10" imageSrc="/images/call-us.png">
                         <p className='text-[#212121] font-medium'>Call us</p>
-                        <p className='text-[#8A8A8A] text-sm'>Mon - Fri from 10am to 5pm</p>
+                        <p className='text-[#8A8A8A] text-sm'>Mon - Fri from 10am to 8pm</p>
                         <p className='mt-3 text-sm text-[#212121]'>
                             +91 76785 58049
                         </p>
